0a2755e88e44d58b63c4460614531bc5e49a8641
AntarProgrammer <antarmitra33@gmail.com>
2023-09-27 12:44:17 +0600

    When we donate something, then the donated cards will be added to the donation page, if the length of the added cards is more than 4, then the remaining cards will not show the cards after the  numbered 1, 2, 3, 4. Then a show All button will appear if you click on that button it will show the rest of the data on the page

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { PieChart, Pie, Cell } from "recharts";
 
-const Statistics = () => {
+
+export default function Statistics() {
+    const [totalPercent, setTotalPercent] = useState([]);
+
+    useEffect(() => {
+        const donationItems = JSON.parse(localStorage.getItem('donation'));
+
+        if (donationItems) {
+            const totalData = 12;
+            const totalDonation = donationItems.length;
+            let per = (totalDonation / totalData) * 100;
+            setTotalPercent(per)
+        }
+    }, [])
+
+    const data = [
+        { name: "Group A", value: 100 - totalPercent },
+        { name: "Group B", value: totalPercent }
+    ];
+
+    const COLORS = ["#FF444A", "#00C49F"];
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({
+        cx,
+        cy,
+        midAngle,
+        innerRadius,
+        outerRadius,
+        percent
+    }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text
+                x={x}
+                y={y}
+                fill="white"
+                textAnchor={x > cx ? "start" : "end"}
+                dominantBaseline="central"
+            >
+                ${(percent * 100).toFixed(1)}%
+            </text>
+        );
+    };
     return (
         <div>
-            This is Statistics
+            <div className="flex justify-center">
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={data}
+                        cx={200}
+                        cy={200}
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={150}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                </PieChart>
+            </div>
+
+            <div className="flex gap-5 md:gap-20 justify-center flex-col md:flex-row">
+                <div className="flex gap-5 items-center">
+                    <p className="text-lg">Your Donation</p>
+                    <div className="h-3 w-24 rounded-sm bg-[#FF444A]"></div>
+                </div>
+                <div className="flex gap-5 items-center">
+                    <p className="text-lg">Total Donation</p>
+                    <div className="h-3 w-24 rounded-sm bg-[#00C49F]"></div>
+
+                </div>
+            </div>
         </div>
     );
-};
+}
 
-export default Statistics;
